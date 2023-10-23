540b952edb4a6df22c434dbf8be26fd3216eeb24
Harry-Cosmos <harry.cosmonaut@gmail.com>
2023-10-23 00:15:41 -0400

    trying to build differently for vercel

--- client/webpack/prod.js
+++ client/webpack/prod.js
@@ -28,7 +28,7 @@ module.exports = merge(base, {
   },
   plugins: [
     new CopyWebpackPlugin([{ from: "src/assets", to: "src/assets" }]),
-    new CopyWebpackPlugin([{ from: "scripts", to: "scripts" }]),
-    new CopyWebpackPlugin([{ from: "style.css", to: "style.css" }]),
+    // new CopyWebpackPlugin([{ from: "scripts", to: "scripts" }]),
+    // new CopyWebpackPlugin([{ from: "style.css", to: "style.css" }]),
   ],
 });
