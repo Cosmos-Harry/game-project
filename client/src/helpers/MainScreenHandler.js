04449fcd55f199745062c2ccbc99fcd5f7501f46
Harry-Cosmos <harry.cosmonaut@gmail.com>
2023-10-24 00:47:50 -0400

    changed for mobile

--- client/src/helpers/MainScreenHandler.js
+++ client/src/helpers/MainScreenHandler.js
@@ -1,86 +1,86 @@
 export default class MainScreenHandler {
   constructor(scene) {
-    this.buildMainScreenText = () => {
+    // this.buildMainScreenText = () => {
       // scene.play = scene.add
       //   .text(570, 200, "PLAY")
       //   .setFontSize(50)
       //   .setFontFamily("Trebuchet MS")
-      scene.play = scene.add.text(60, 400, "PLAY", {
-        fontFamily: "Gluten",
-        fontSize: "100px",
-        stroke: "#ad4d42",
-        strokeThickness: "10",
-        shadow: {
-          offsetX: 4,
-          offsetY: 4,
-          blur: 10,
-          stroke: true,
-          fill: true,
-        },
-      });
+    //   scene.play = scene.add.text(60, 400, "PLAY", {
+    //     fontFamily: "Gluten",
+    //     fontSize: "100px",
+    //     stroke: "#ad4d42",
+    //     strokeThickness: "10",
+    //     shadow: {
+    //       offsetX: 4,
+    //       offsetY: 4,
+    //       blur: 10,
+    //       stroke: true,
+    //       fill: true,
+    //     },
+    //   });
 
-      scene.play.setInteractive();
+    //   scene.play.setInteractive();
 
-      scene.play.on("pointerdown", () => {
-        scene.scene.start("Game");
-      });
+    //   scene.play.on("pointerdown", () => {
+    //     scene.scene.start("Game");
+    //   });
 
-      scene.play.on("pointerover", () => {
-        scene.play.setShadow(6, 6, "rgba(0,0,0,3.5)", 14, true, true);
-      });
+    //   scene.play.on("pointerover", () => {
+    //     scene.play.setShadow(6, 6, "rgba(0,0,0,3.5)", 14, true, true);
+    //   });
 
-      scene.play.on("pointerout", () => {
-        scene.play.setShadow(4, 4, "rgba(0,0,0,3.5)", 10, true, true);
-      });
+    //   scene.play.on("pointerout", () => {
+    //     scene.play.setShadow(4, 4, "rgba(0,0,0,3.5)", 10, true, true);
+    //   });
 
-      scene.settings = scene.add.text(120, 520, "SETTINGS  ", {
-        fontFamily: "Gluten",
-        fontSize: "30px",
-        stroke: "#ad4d42",
-        strokeThickness: "10",
-        shadow: {
-          offsetX: 4,
-          offsetY: 4,
-          blur: 10,
-          stroke: true,
-          fill: true,
-        },
-      });
+    //   scene.settings = scene.add.text(120, 520, "SETTINGS  ", {
+    //     fontFamily: "Gluten",
+    //     fontSize: "30px",
+    //     stroke: "#ad4d42",
+    //     strokeThickness: "10",
+    //     shadow: {
+    //       offsetX: 4,
+    //       offsetY: 4,
+    //       blur: 10,
+    //       stroke: true,
+    //       fill: true,
+    //     },
+    //   });
 
-      scene.settings.setInteractive();
+    //   scene.settings.setInteractive();
 
-      scene.settings.on("pointerover", () => {
-        scene.settings.setShadow(6, 6, "rgba(0,0,0,3.5)", 14, true, true);
-      });
+    //   scene.settings.on("pointerover", () => {
+    //     scene.settings.setShadow(6, 6, "rgba(0,0,0,3.5)", 14, true, true);
+    //   });
 
-      scene.settings.on("pointerout", () => {
-        scene.settings.setShadow(4, 4, "rgba(0,0,0,3.5)", 10, true, true);
-      });
+    //   scene.settings.on("pointerout", () => {
+    //     scene.settings.setShadow(4, 4, "rgba(0,0,0,3.5)", 10, true, true);
+    //   });
 
-      scene.source = scene.add.text(290, 20, "SOURCE", {
-        fontFamily: "Gluten",
-        fontSize: "22px",
-        stroke: "#ad4d42",
-        strokeThickness: "4",
-        shadow: {
-          offsetX: 4,
-          offsetY: 4,
-          blur: 4,
-          stroke: true,
-          fill: true,
-        },
-      });
+    //   scene.source = scene.add.text(290, 20, "SOURCE", {
+    //     fontFamily: "Gluten",
+    //     fontSize: "22px",
+    //     stroke: "#ad4d42",
+    //     strokeThickness: "4",
+    //     shadow: {
+    //       offsetX: 4,
+    //       offsetY: 4,
+    //       blur: 4,
+    //       stroke: true,
+    //       fill: true,
+    //     },
+    //   });
 
-      scene.source.setInteractive();
+    //   scene.source.setInteractive();
 
-      scene.source.on("pointerover", () => {
-        scene.source.setShadow(6, 6, "rgba(0,0,0,3.5)", 8, true, true);
-      });
+    //   scene.source.on("pointerover", () => {
+    //     scene.source.setShadow(6, 6, "rgba(0,0,0,3.5)", 8, true, true);
+    //   });
 
-      scene.source.on("pointerout", () => {
-        scene.source.setShadow(4, 4, "rgba(0,0,0,3.5)", 4, true, true);
-      });
-    };
+    //   scene.source.on("pointerout", () => {
+    //     scene.source.setShadow(4, 4, "rgba(0,0,0,3.5)", 4, true, true);
+    //   });
+    // };
 
     this.buildUI = () => {
       this.buildMainScreenText();
--- client/src/scenes/mainScreen.js
+++ client/src/scenes/mainScreen.js
@@ -12,6 +12,83 @@ export default class MainScreen extends Phaser.Scene {
 
   create() {
     this.add.image(200, 400, "background");
+
+    const play = this.add.text(60, 400, "PLAY", {
+      fontFamily: "Gluten",
+      fontSize: "100px",
+      stroke: "#ad4d42",
+      strokeThickness: "10",
+      shadow: {
+        offsetX: 4,
+        offsetY: 4,
+        blur: 10,
+        stroke: true,
+        fill: true,
+      },
+    });
+
+    play.setInteractive();
+
+    play.on("pointerdown", () => {
+      this.scene.start("Game");
+    });
+
+    play.on("pointerover", () => {
+      play.setShadow(6, 6, "rgba(0,0,0,3.5)", 14, true, true);
+    });
+
+    play.on("pointerout", () => {
+      play.setShadow(4, 4, "rgba(0,0,0,3.5)", 10, true, true);
+    });
+
+    const settings = this.add.text(120, 520, "SETTINGS  ", {
+      fontFamily: "Gluten",
+      fontSize: "30px",
+      stroke: "#ad4d42",
+      strokeThickness: "10",
+      shadow: {
+        offsetX: 4,
+        offsetY: 4,
+        blur: 10,
+        stroke: true,
+        fill: true,
+      },
+    });
+
+    settings.setInteractive();
+
+    settings.on("pointerover", () => {
+      settings.setShadow(6, 6, "rgba(0,0,0,3.5)", 14, true, true);
+    });
+
+    settings.on("pointerout", () => {
+      settings.setShadow(4, 4, "rgba(0,0,0,3.5)", 10, true, true);
+    });
+
+    const source = this.add.text(290, 20, "SOURCE", {
+      fontFamily: "Gluten",
+      fontSize: "22px",
+      stroke: "#ad4d42",
+      strokeThickness: "4",
+      shadow: {
+        offsetX: 4,
+        offsetY: 4,
+        blur: 4,
+        stroke: true,
+        fill: true,
+      },
+    });
+
+    source.setInteractive();
+
+    source.on("pointerover", () => {
+      source.setShadow(6, 6, "rgba(0,0,0,3.5)", 8, true, true);
+    });
+
+    source.on("pointerout", () => {
+      source.setShadow(4, 4, "rgba(0,0,0,3.5)", 4, true, true);
+    });
+
     this.MainScreenHandler = new MainScreenHandler(this);
     this.MainScreenHandler.buildUI();
   }
