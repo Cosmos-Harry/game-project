01ec233aae4bfbb158b39336e6083b5d2c786840
Harry-Cosmos <harry.cosmonaut@gmail.com>
2023-11-05 16:41:56 +0530

    Added cloud and class Obstacles to make the code concise

--- client/src/scenes/game.js
+++ client/src/scenes/game.js
@@ -10,6 +10,7 @@ export default class Game extends Phaser.Scene {
     this.load.image("background", "src/assets/background.png");
     this.load.image("player", "src/assets/player.png");
     this.load.image("playerHit", "src/assets/player-hit.png");
+    this.load.image("cloud", "src/assets/clouds.png");
     this.load.spritesheet("shuttle", "src/assets/shuttle.PNG", {
       frameWidth: 269, // Width of each frame
       frameHeight: 648, // Height of each frame
@@ -17,87 +18,70 @@ export default class Game extends Phaser.Scene {
   }
 
   create() {
-    // Add background image
-    const bg = this.add.image(200, 400, "background");
-    bg.setAlpha(0.8); // Changed opacity
-
-    //Add player image as "physics.add.sprite" to make the gravity/bounds work
-    // const player = this.physics.add.sprite(200, 450, "player");
-    let player = this.createPlayer(200, 450);
-    let playerHit = null;
-
-    this.physics.world.setBounds(0, 0, 400, 800); // Set world bounds
-    player.setCollideWorldBounds(true); // Enable collisions between the player and the world bounds
-    player.setInteractive(); // Enable input for the player sprite
-
-    // Set up event listeners for mouse input
-    // Set the offset to avoid the player being covered by touch/cursor
-    const offset = { x: 0, y: -80 }; // Adjust the offset values as needed
-
-    this.input.on("pointermove", (pointer) => {
-      // Set the player's position to the cursor's position with an offset
-      player.x = pointer.x + offset.x;
-      player.y = pointer.y + offset.y;
-    });
-
-    //Text element to display the timer
-    const timerText = this.add.text(110, 20, "Score: 0", {
-      fontFamily: "Gluten",
-      fontSize: "40px",
-      fill: "#ffffff",
-    });
-
-    let elapsedTime = 0; // Initialize the elapsed time
-    let shuttleTimer = 0;
-    const shuttleInterval = 2;
     let numberOfShuttles = 1;
-    let shuttle;
     let isGameOver = false;
     let gameOverText;
+    let elapsedTime = 0; // Initialize the elapsed time
+    let playerHit = null;
 
-    // Create a timer
-    const timer = this.time.addEvent({
-      delay: 1000, // 1 second
-      repeat: -1, // 60 times (60 seconds)
-      // callback: yourCallbackFunction, // The function to call each second
-      callback: () => {
-        elapsedTime++;
-        timerText.setText(`Score: ${elapsedTime}`);
-
-        // if (elapsedTime === 2) {
-        //   this.shuttleCallback();
-        // }
-        shuttleTimer++;
-        if (shuttleTimer >= shuttleInterval) {
-          this.shuttleCallback();
-          shuttleTimer = 0;
-        }
-
-        if (elapsedTime === 20) {
-          numberOfShuttles = 2;
-        }
-      },
-    });
-
+    this.bgCallback = () => {
+      new Obstacle(this).createSprite(
+        "background",
+        200,
+        400,
+        1,
+        null,
+        null,
+        0.8,
+        0
+      );
+    };
+    this.playerCallback = () => {
+      const player = new Obstacle(this).createSprite(
+        "player",
+        200,
+        450,
+        0.05,
+        0,
+        0,
+        1,
+        1
+      );
+      this.physics.world.setBounds(0, 0, 400, 800);
+      player.setCollideWorldBounds(true);
+      player.setInteractive();
+      return player;
+    };
+    this.cloudCallback = () => {
+      const randomY = Phaser.Math.Between(0, this.physics.world.bounds.width);
+      new Obstacle(this).createSprite(
+        "cloud",
+        1400,
+        randomY,
+        0.4,
+        -100,
+        0,
+        1,
+        10
+      );
+    };
     this.shuttleCallback = () => {
       for (let i = 0; i < numberOfShuttles; i++) {
-        // Add shuttle spritesheet
         // Randomly select the X position within the game world bounds
         const randomX = Phaser.Math.Between(0, this.physics.world.bounds.width);
-        shuttle = this.physics.add.sprite(randomX, 800, "shuttle");
-        shuttle.setScale(0.2);
-        shuttle.setGravityY(-300); // Set gravity of the shuttle
-        // create animation of the shuttle spritesheet
-        this.anims.create({
-          key: "fly",
-          frames: this.anims.generateFrameNumbers("shuttle", {
-            start: 0,
-            end: 5,
-          }),
-          frameRate: 10,
-          repeat: -1,
-        });
-        shuttle.play("fly"); // play the animation
+        const shuttle = new Obstacle(this).createSprite(
+          "shuttle",
+          randomX,
+          800,
+          0.2,
+          0,
+          -300,
+          1,
+          2
+        );
+
+        new Obstacle(this).createAnimation("fly", "shuttle", 0, 5, 10, -1);
+        shuttle.play("fly");
 
         this.physics.add.overlap(player, shuttle, () => {
           if (!isGameOver) {
@@ -168,7 +152,7 @@ export default class Game extends Phaser.Scene {
               playerHit.destroy();
 
               // Recreate the player
-              player = this.createPlayer(200, 450);
+              player = this.playerCallback();
 
               this.scene.restart();
             });
@@ -177,18 +161,83 @@ export default class Game extends Phaser.Scene {
       }
     };
 
+    //Text element to display the timer
+    const timerText = this.add.text(110, 20, "Score: 0", {
+      fontFamily: "Gluten",
+      fontSize: "40px",
+      fill: "#ffffff",
+    });
+    timerText.setDepth(1);
+
+    // Create a timer
+    const timer = this.time.addEvent({
+      delay: 1000, // 1 second
+      repeat: -1, // 60 times (60 seconds)
+      // callback: yourCallbackFunction, // The function to call each second
+      callback: () => {
+        elapsedTime++;
+        timerText.setText(`Score: ${elapsedTime}`);
+
+        if (elapsedTime % 10 === 0) {
+          this.cloudCallback();
+        }
+        if (elapsedTime % 2 === 0) {
+          this.shuttleCallback();
+        }
+
+        if (elapsedTime === 20) {
+          numberOfShuttles = 2;
+        }
+      },
+    });
+
+    //Add player image as "physics.add.sprite" to make the gravity/bounds work
+    // const player = this.physics.add.sprite(200, 450, "player");
+    this.bgCallback();
+    let player = this.playerCallback();
+
+    this.physics.world.setBounds(0, 0, 400, 800); // Set world bounds
+
+    // Set up event listeners for mouse input
+    // Set the offset to avoid the player being covered by touch/cursor
+    const offset = { x: 0, y: -80 }; // Adjust the offset values as needed
+
+    this.input.on("pointermove", (pointer) => {
+      // Set the player's position to the cursor's position with an offset
+      player.x = pointer.x + offset.x;
+      player.y = pointer.y + offset.y;
+    });
+
     this.GameScreenHandler = new GameScreenHandler(this);
     this.GameScreenHandler.buildUI();
   }
 
   update() {}
-  // Create a player sprite
-  createPlayer(x, y) {
-    const player = this.physics.add.sprite(x, y, "player");
-    player.setScale(0.05);
-    this.physics.world.setBounds(0, 0, 400, 800);
-    player.setCollideWorldBounds(true);
-    player.setInteractive();
-    return player;
+}
+class Obstacle {
+  constructor(scene) {
+    this.scene = scene;
+  }
+
+  createSprite(givenSprite, x, y, scale, gravityX, gravityY, opacity, depth) {
+    const sprite = this.scene.physics.add.sprite(x, y, givenSprite);
+    sprite.setScale(scale);
+    sprite.setGravityX(gravityX);
+    sprite.setGravityY(gravityY);
+    sprite.setAlpha(opacity);
+    sprite.setDepth(depth);
+    return sprite;
+  }
+
+  createAnimation(givenKey, givenSprite, start, end, frameRate, repeat) {
+    this.scene.anims.create({
+      key: givenKey,
+      frames: this.scene.anims.generateFrameNumbers(givenSprite, {
+        start: start,
+        end: end,
+      }),
+      frameRate: frameRate,
+      repeat: repeat,
+    });
   }
 }
